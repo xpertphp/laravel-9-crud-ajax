d84d5bf46344295720c131afb23e8b292cdf3c7c
unknown <xpertphp2018@gmail.com>
2022-03-29 21:59:33 +0530

    add project

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('student','App\Http\Controllers\StudentController@index');
+Route::post('student','App\Http\Controllers\StudentController@store')->name('student.store');
+Route::get('student/{id}/edit', 'App\Http\Controllers\StudentController@edit')->name('student.edit');
+Route::post('student/update', 'App\Http\Controllers\StudentController@update')->name('student.update');
+Route::get('student/{id}/delete', 'App\Http\Controllers\StudentController@destroy')->name('student.delete');
